1c3954ada01c53773729ac5d1c541ecc0b2ba167
Sam Agnew <sagnew@eden.rutgers.edu>
2016-05-03 12:02:40 -0400

    Rename variables and clean up code

--- HowManyPeopleAreInSpaceRightNow/ViewController.swift
+++ HowManyPeopleAreInSpaceRightNow/ViewController.swift
@@ -11,22 +11,17 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController {
-    @IBOutlet var pictureOfTheDayOutlet: UIImageView!
+    @IBOutlet var marsPhotoImageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         let dateString = getYesterdayDateString()
         Alamofire.request(.GET, "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos", parameters: ["api_key": "DEMO_KEY", "earth_date": dateString])
             .responseJSON { response in
-                print(response.request)  // original URL request
-                print(response.response) // URL response
-                print(response.data)     // server data
-                print(response.result)   // result of response serialization
-                
-                if let result = response.result.value {
+              if let result = response.result.value {
                     let json = JSON(result)
                     if let imageUrl = json["photos"][0]["img_src"].string {
-                        self.load_image(imageUrl.stringByReplacingOccurrencesOfString("http", withString: "https"))
+                        self.loadImage(imageUrl.stringByReplacingOccurrencesOfString("http", withString: "https"))
                     }
                 }
         }
@@ -37,15 +32,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func load_image(urlString:String)
-    {
+    func loadImage(urlString:String) {
         let imgURL: NSURL = NSURL(string: urlString)!
         let request: NSURLRequest = NSURLRequest(URL: imgURL)
         NSURLConnection.sendAsynchronousRequest(
             request, queue: NSOperationQueue.mainQueue(),
             completionHandler: {(response: NSURLResponse?, data: NSData?, error: NSError?) -> Void in
                 if error == nil {
-                    self.pictureOfTheDayOutlet.image = UIImage(data: data!)
+                    self.marsPhotoImageView.image = UIImage(data: data!)
                 }
         })
         
